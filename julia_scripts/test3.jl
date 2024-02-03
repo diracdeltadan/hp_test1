1d09e52098adfb2bf8774cc273d8cf7b75cca76c
dannyboy <jdstevens03@gmail.com>
2024-02-03 14:14:51 -0600

    updated test3.jl

--- julia_scripts/test3.jl
+++ julia_scripts/test3.jl
@@ -5,10 +5,10 @@ A=rand(10)
 
 # print(A)
 
-#using CUDA
+using CUDA
 
-#CA = CUDA.rand(Float32,200);
+CA = CUDA.rand(Float32,2_000_000);
 
-#sca = sum(CA);
+sca = sum(CA);
 
-#println(sca)
+println(sca)
