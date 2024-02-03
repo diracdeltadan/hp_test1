c3e99af1dabee29a697580f3cbb78699b6765823
dannyboy <jdstevens03@gmail.com>
2024-02-03 14:11:32 -0600

    initial commit test1

---
+++ hello world.py
@@ -0,0 +1,5 @@
+print('hello world')
+import numpy as np
+x=np.arange(10)
+for j in x:
+    print(j)
---
+++ julia_scripts/test3.jl
@@ -0,0 +1,14 @@
+using LinearAlgebra
+using BenchmarkTools
+
+A=rand(10)
+
+# print(A)
+
+#using CUDA
+
+#CA = CUDA.rand(Float32,200);
+
+#sca = sum(CA);
+
+#println(sca)
